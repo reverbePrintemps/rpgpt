bb3f1059a569c51fd438e2a50202a9466a6d16cf
reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>
2023-06-19 14:57:51 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,51 @@
-'use client'
+"use client";
 
-import { useChat } from 'ai/react'
+import { Message } from "ai";
+import { useChat } from "ai/react";
+import { useEffect } from "react";
+
+const initMessages = [
+  {
+    id: "0",
+    role: "system",
+    content:
+      "You are a text-based adventure game master. The game will take place in the Star Wars universe and will last a maximum of three rounds. Each round, you will present the player with one prompt and two actions to choose from. Once the player has provided you with an action, you will provide a new prompt based on their choice. The game ends either with the player's death or with a successful completion of the game's objective.",
+  },
+  {
+    id: "1",
+    role: "user",
+    content: "I'm ready to play!",
+  },
+] as Message[];
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat()
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    setMessages,
+    reload,
+  } = useChat();
+
+  useEffect(() => {
+    setMessages(initMessages);
+    reload();
+  }, []);
 
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.length > 0
-        ? messages.map(m => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === 'user' ? 'User: ' : 'AI: '}
-              {m.content}
-            </div>
-          ))
+        ? messages.map((m) => {
+            return (
+              m.id > "1" && (
+                <div key={m.id} className="whitespace-pre-wrap">
+                  {m.role === "user" ? "User: " : "AI: "}
+                  {m.content}
+                </div>
+              )
+            );
+          })
         : null}
 
       <form onSubmit={handleSubmit}>
@@ -25,5 +57,5 @@ export default function Chat() {
         />
       </form>
     </div>
-  )
+  );
 }
