f7269a57d87f97ae41b4d52867164c7a9896b47c
reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>
2023-06-24 15:37:08 +0200

    Refactor /game page to fix bug

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -8,17 +8,6 @@ import { scrollToBottom } from "../utils/general";
 import { Round } from "../components/Round";
 import { useChat } from "ai/react";
 
-// TODO
-
-// - [ ] Round probably doesn't need both an onClick and an onSubmit. Unify.
-//2 - [ ] Rely on something else than messages to scroll to bottom to prevent slight jitter when messages are received but not yet able to be parsed and rendered
-
-/* // TODO Use when implementing first person dialogue mode
-content: `You are a text-based adventure game master. You will assume the role of various characters throughout the game and engage solely in dialogue with the player. The player will have the choice of various dialogue responses. All of your responses should be strictly formatted like so: ${JSON.stringify(
-      roundExample
-    )}.`,
-*/
-
 export default function Page() {
   const ref = useRef<HTMLDivElement>(null);
   const [rounds, setRounds] = useState<Round[]>([]);
@@ -46,9 +35,25 @@ export default function Page() {
   useEffect(() => {
     process.env.NODE_ENV === "development" && console.log("messages", messages);
     setMessage(messages[messages.length - 1]);
-    // TD2
     scrollToBottom(ref, "smooth", "end");
   }, [messages]);
+
+  useEffect(() => {
+    if (latestRound) {
+      if (latestRound.id === rounds[rounds.length - 1]?.id) {
+        // Replace the latest round in rounds array
+        setRounds((prevRounds) => {
+          const updatedRounds = [...prevRounds];
+          updatedRounds[updatedRounds.length - 1] = latestRound;
+          return updatedRounds;
+        });
+      } else {
+        // Append the latest round to the rounds array
+        setRounds((prevRounds) => [...prevRounds, latestRound]);
+      }
+      setLatestRound(null);
+    }
+  }, [latestRound, setLatestRound, rounds]);
 
   const typingPlaceholders = typingEffect({
     cursor: true,
@@ -59,72 +64,61 @@ export default function Page() {
     sequence: placeholderPrompts,
   });
 
+  const handleRoundChoiceSelected = (input: string) => {
+    setInput(input);
+  };
+
+  const handleRoundSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    setIsLoading(true);
+    handleSubmit(e);
+  };
+
+  const handleFreeTextSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    setIsLoading(true);
+    handleSubmit(e);
+  };
+
+  const renderRounds = () => {
+    return rounds.map((round) => (
+      <Round
+        round={round}
+        key={round.id}
+        onChoiceSelected={handleRoundChoiceSelected}
+        onSubmit={handleRoundSubmit}
+      />
+    ));
+  };
+
   return (
     <div ref={ref} className="scroll-m-52">
       <h1 className="text-5xl font-bold">Game</h1>
-      {rounds.map((round) => (
-        <Round
-          round={round}
-          key={round.id}
-          onChoiceSelected={(input) => {
-            setInput(input);
-          }}
-          onSubmit={(e) => {
-            setIsLoading(true);
-            handleSubmit(e);
-          }}
-        />
-      ))}
+      {renderRounds()}
       {isLoading && <p>Loading...</p>}
-      {latestRound && (
-        <>
-          <Round
-            round={latestRound}
-            onChoiceSelected={(input) => {
-              setInput(input);
-              setRounds((prev) => [...prev, latestRound]);
-              setLatestRound(null);
-            }}
-            onSubmit={(e) => {
-              handleSubmit(e);
-              setIsLoading(true);
-              setRounds((prev) => [...prev, latestRound]);
-              setLatestRound(null);
-            }}
-          />
-          {latestRound.free_text && (
-            <form
-              className="flex flex-col mt-4"
-              onSubmit={(e) => {
-                setIsLoading(true);
-                handleSubmit(e);
-              }}
+      {latestRound && latestRound.free_text && (
+        <form className="flex flex-col mt-4" onSubmit={handleFreeTextSubmit}>
+          <div className="left-0 flex">
+            <AutogrowingInput
+              onChange={handleInputChange}
+              placeholder={typingPlaceholders}
+              value={input}
+              onEnter={() =>
+                handleRoundSubmit(
+                  new Event("submit", {
+                    cancelable: true,
+                    bubbles: true,
+                  }) as any
+                )
+              }
+            />
+            <button
+              className="bg-slate-300 text-stone-800 p-2 rounded-lg ml-4 font-bold disabled:opacity-50"
+              type="submit"
+              disabled={isWriting || input === ""}
             >
-              <div className="left-0 flex">
-                <AutogrowingInput
-                  onChange={handleInputChange}
-                  placeholder={typingPlaceholders}
-                  value={input}
-                  onEnter={() =>
-                    handleSubmit(
-                      new Event("submit", {
-                        cancelable: true,
-                        bubbles: true,
-                      }) as any
-                    )
-                  }
-                />
-                <button
-                  className="bg-slate-300 text-stone-800 p-2 rounded-lg ml-4 font-bold disabled:opacity-50"
-                  type="submit"
-                  disabled={isWriting || input === ""}
-                >
-                  Send
-                </button>
-              </div>
-            </form>
-          )}
-        </>
+              Send
+            </button>
+          </div>
+        </form>
       )}
     </div>
   );
