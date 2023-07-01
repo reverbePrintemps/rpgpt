a05122c9866b2242a4d82c5892675f3bef1a491e
reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>
2023-07-02 01:54:54 +0200

    Improve Donate banner

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -27,7 +27,7 @@ export const Navbar = ({ children, className, style }: HeaderProps) => {
         </button>
       </div>
       {shouldShowBanner && (
-        <div className="alert alert-warning mt-2 flex max-w-4xl mx-auto">
+        <div className="alert alert-warning mt-2 flex max-w-4xl">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="stroke-current shrink-0 h-6 w-6"
@@ -41,22 +41,27 @@ export const Navbar = ({ children, className, style }: HeaderProps) => {
               d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
             />
           </svg>
-          <span>
-            Due to a sudden and unexpected increase in popularity for this app,
-            I am unable to incur the api costs for much longer. Consider
-            donating to help keep this app alive. ❤️
-          </span>
-          <div className="flex flex-col">
-            <button className="btn btn-md " onClick={handleBannerDismiss}>
-              Dismiss
-            </button>
-            <Link
-              href="https://www.buymeacoffee.com/jandreo"
-              target="_blank"
-              className="block mt-2"
-            >
-              <button className="btn btn-md btn-primary">Donate</button>
-            </Link>
+          <div className="flex flex-col items-end">
+            <p className="ml-2">
+              Due to a sudden and unexpected increase in popularity for this
+              app, I am unable to incur the api costs for much longer. Consider
+              donating to help keep this app alive. ❤️
+            </p>
+            <div className="flex mt-4">
+              <button
+                className="btn btn-md btn-ghost"
+                onClick={handleBannerDismiss}
+              >
+                Dismiss
+              </button>
+              <Link
+                href="https://www.buymeacoffee.com/jandreo"
+                target="_blank"
+                className="block"
+              >
+                <button className="btn btn-md btn-primary">Donate</button>
+              </Link>
+            </div>
           </div>
         </div>
       )}
