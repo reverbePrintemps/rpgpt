8f68b543d08ae42d599d96ac74e3587253bc9a16
reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>
2023-07-02 23:53:45 +0200

    Add real time usage data to header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ReactNode, CSSProperties, useState } from "react";
+import { ReactNode, CSSProperties, useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
@@ -9,10 +9,91 @@ interface HeaderProps {
   style?: CSSProperties;
 }
 
+const getStartOfCurrentMonth = () => {
+  const now = new Date();
+  return new Date(now.getFullYear(), now.getMonth(), 1)
+    .toISOString()
+    .split("T")[0];
+};
+
+const getEndOfCurrentMonth = () => {
+  const now = new Date();
+  return new Date(now.getFullYear(), now.getMonth() + 1, 0)
+    .toISOString()
+    .split("T")[0];
+};
+
 export const Navbar = ({ children, className, style }: HeaderProps) => {
   const pathName = usePathname();
   const [shouldShowBanner, setShouldShowBanner] = useState(true);
   const handleBannerDismiss = () => setShouldShowBanner(false);
+  const [hardLimit, setHardLimit] = useState<number>();
+  const [usage, setUsage] = useState<number>();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const getSub = async () => {
+      const res = await fetch(
+        "https://api.openai.com/dashboard/billing/subscription",
+        {
+          credentials: "omit",
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/114.0",
+            Accept: "*/*",
+            "Accept-Language": "en-GB,en;q=0.5",
+            Authorization:
+              "Bearer sess-aioet4W94SS6KRBnesQ2ObsPSNcmXqCloKJDU5nh",
+            "Access-Control-Allow-Credentials": "true",
+            "OpenAI-Organization": "org-PA9zwN9PpPNCF4kwiwurep1b",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "Sec-GPC": "1",
+          },
+          referrer: "https://platform.openai.com/",
+          method: "GET",
+          mode: "cors",
+        }
+      );
+      const data = await res.json();
+      console.log("subscription", data);
+      return data;
+    };
+    const getUsage = async () => {
+      const res = await fetch(
+        `https://api.openai.com/dashboard/billing/usage?end_date=${getEndOfCurrentMonth()}&start_date=${getStartOfCurrentMonth()}`,
+        {
+          credentials: "omit",
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/114.0",
+            Accept: "*/*",
+            "Accept-Language": "en-GB,en;q=0.5",
+            Authorization:
+              "Bearer sess-aioet4W94SS6KRBnesQ2ObsPSNcmXqCloKJDU5nh",
+            "OpenAI-Organization": "org-PA9zwN9PpPNCF4kwiwurep1b",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "Sec-GPC": "1",
+          },
+          referrer: "https://platform.openai.com/",
+          method: "GET",
+          mode: "cors",
+        }
+      );
+      const data = await res.json();
+      console.log("usage", data);
+      return data;
+    };
+    Promise.all([getSub(), getUsage()]).then(([sub, usage]) => {
+      if (usage.total_usage) setUsage(Math.round(usage.total_usage) / 100);
+      if (sub.hard_limit_usd) setHardLimit(sub.hard_limit_usd);
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <nav className="navbar bg-neutral text-neutral-content flex flex-col">
@@ -49,6 +130,28 @@ export const Navbar = ({ children, className, style }: HeaderProps) => {
               existence and further development, please consider making a small
               donation by buying me a tea. ❤️
             </p>
+            {loading && (
+              <span className="loading loading-spinner loading-sm mt-4" />
+            )}
+            {usage && hardLimit && (
+              <div className="stats bg-info mt-4">
+                <div className="stat">
+                  <div className="stat-title text-neutral">Current usage</div>
+                  <div className="stat-value text-error-content">${usage}</div>
+                  <div className="stat-desc text-neutral">
+                    <div>From {getStartOfCurrentMonth()} </div>
+                    <div>to {getEndOfCurrentMonth()}</div>
+                  </div>
+                </div>
+                <div className="stat">
+                  <div className="stat-title text-neutral">Donations</div>
+                  <div className="stat-value text-success-content">$27</div>
+                  <div className="stat-desc text-neutral">
+                    <p>From 3 donors</p>
+                  </div>
+                </div>
+              </div>
+            )}
             <div className="flex mt-4">
               <button
                 className="btn btn-md btn-ghost"
@@ -61,7 +164,7 @@ export const Navbar = ({ children, className, style }: HeaderProps) => {
                 target="_blank"
                 className="block"
               >
-                <button className="btn btn-md btn-primary ml-2">
+                <button className="btn btn-md btn-primary">
                   🍵 Buy me a tea
                 </button>
               </Link>
