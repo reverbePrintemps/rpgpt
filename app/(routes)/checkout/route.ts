45d33dbcc80482bb7dce4e0471838a315b959c73
reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>
2023-08-11 17:26:29 +0200

    Use correct product price per environment

--- app/(routes)/checkout/route.ts
+++ app/(routes)/checkout/route.ts
@@ -1,14 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { stripe } from "@/app/stripe/stripe";
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest) {
   const cri = req.nextUrl.searchParams.get("cri");
+  const priceId = ["test", "development"].includes(process.env.NODE_ENV)
+    ? "price_1NZa6uEhHgoFRl3LRH0oRVki"
+    : process.env.NODE_ENV === "production"
+    ? "price_1NdwlbEhHgoFRl3LMqp5vN8S"
+    : "";
   const session = await stripe.checkout.sessions.create({
     mode: "subscription",
     payment_method_types: ["card"],
     line_items: [
       {
-        price: "price_1NZa6uEhHgoFRl3LRH0oRVki",
+        price: priceId,
       },
     ],
     success_url: `${process.env.NEXT_PUBLIC_URL}/success`,
