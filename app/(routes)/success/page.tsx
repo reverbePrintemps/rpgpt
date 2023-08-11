700dbc952329fca6c28696c72ab58d6a0d2180d0
reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>
2023-08-11 18:12:59 +0200

    Update success page

--- app/(routes)/success/page.tsx
+++ app/(routes)/success/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import { Button } from "react-daisyui";
+import Link from "next/link";
+
 export default function Page() {
-  return <div>success</div>;
+  return (
+    <div>
+      <h1>Thank you! 🎉</h1>
+      <h2>Your payment has been processed.</h2>
+      <p className="mt-8">Time to go on a new adventure!</p>
+      <Link href="/game">
+        <Button color="primary" className="mt-2">
+          New Game
+        </Button>
+      </Link>
+    </div>
+  );
 }
